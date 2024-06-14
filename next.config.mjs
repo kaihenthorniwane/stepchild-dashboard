51bf07d42228704d242595233dbf595e3a86e504
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-06-13 21:16:16 -0700

    add publish actions in .github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/stepchild-dashboard",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
