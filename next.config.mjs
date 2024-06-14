26837d7ff6defe2372b89c890bec7a4a8336daa7
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-06-13 21:23:14 -0700

    attemp to deploy 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   basePath: "/stepchild-dashboard",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
