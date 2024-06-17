9a47c0afece41dbbbee3dad8d4603db81c59ccde
alexlafetra <alexlafetrathompson@gmail.com>
2024-06-16 20:33:32 -0700

    Adding in webSerial functionality pt.1
    Added in basic port selection, def gonna have some trouble making the ports accessible to different app components

--- src/stepchild_interface/data_functions.tsx
+++ src/stepchild_interface/data_functions.tsx
@@ -1,8 +1,15 @@
 import { StepchildFile } from "@/app/context/FilesContext";
 import { isStepchildFile } from "./helper";
+import StepchildSerialInterface from "./StepchildSerialInterface/interface.js";
 
 export const loadDataFromStepChild = async (): Promise<StepchildFile[]> => {
-  const data: any = "some data"; // This will be replaced by the actual data from stepchild
+
+  const serialInterface = new StepchildSerialInterface();
+  await serialInterface.connect();
+
+  const data : any = await serialInterface.downloadFilesystem();
+
+  // const data: any = "some data"; // This will be replaced by the actual data from stepchild
 
   const parsedData = JSON.parse(data);
 
